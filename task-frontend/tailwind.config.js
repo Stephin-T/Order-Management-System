b48d11151f6ff78716cc90071a9fab7658a1d325
Stephin T <86826149+Stephin-T@users.noreply.github.com>
2024-09-25 10:41:40 +0530

    Add files via upload

---
+++ task-frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Add any other paths where you use Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
